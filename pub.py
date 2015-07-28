2915bcec5435a8a536dae6348328d3da2834d0f0
Tetsuya Shinone <tetsuya.shinone@photosynth.co.jp>
2015-07-28 18:26:03 +0900

    add some test source files

---
+++ pub.py
@@ -0,0 +1,26 @@
+# coding=utf8
+
+import paho.mqtt.client as paho
+
+def on_connect(mqttc, obj, rc):
+    mqttc.subscribe("$SYS/#", 0)
+    print("rc: "+str(rc))
+
+def on_message(mqttc, obj, msg):
+    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+
+def on_publish(mqttc, obj, mid):
+    print("mid: "+str(mid))
+
+def on_log(mqttc, obj, level, string):
+    print(string)
+
+if __name__ == '__main__':
+    mqttc = paho.Client()
+    mqttc.on_message = on_message
+    mqttc.on_connect = on_connect
+    mqttc.on_publish = on_publish
+
+    mqttc.connect("192.168.33.10", 1883, 60)
+
+    mqttc.publish("my/topic/string", "hello world", 1)
---
+++ sub.py
@@ -0,0 +1,31 @@
+# coding=utf8
+
+import paho.mqtt.client as paho
+
+def on_connect(mqttc, obj, rc):
+    mqttc.subscribe("$SYS/#", 0)
+    print("rc: "+str(rc))
+
+def on_message(mqttc, obj, msg):
+    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+
+def on_publish(mqttc, obj, mid):
+    print("mid: "+str(mid))
+
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+
+def on_log(mqttc, obj, level, string):
+    print(string)
+
+if __name__ == '__main__':
+    mqttc = paho.Client()
+    mqttc.on_message = on_message
+    mqttc.on_connect = on_connect
+    mqttc.on_subscribe = on_subscribe
+
+    mqttc.connect("192.168.33.10", 1883, 60)
+
+    mqttc.subscribe("my/topic/string", 0)
+
+    mqttc.loop_forever()
