0bc88399ec61083a43c678e138a79807aec210fc
Tetsuya Shinone <tetsuya.shinone@photosynth.co.jp>
2015-08-26 18:57:03 +0900

    add some account settings

--- pub.py
+++ pub.py
@@ -1,16 +1,18 @@
 # coding=utf8
 
 import paho.mqtt.client as paho
+import sys
+
+topic = "$SYS/#"
 
 def on_connect(mqttc, obj, rc):
-    mqttc.subscribe("$SYS/#", 0)
-    print("rc: "+str(rc))
+    print("rc: " + str(rc))
 
 def on_message(mqttc, obj, msg):
-    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
 def on_publish(mqttc, obj, mid):
-    print("mid: "+str(mid))
+    print("mid: " + str(mid))
 
 def on_log(mqttc, obj, level, string):
     print(string)
@@ -21,6 +23,13 @@ if __name__ == '__main__':
     mqttc.on_connect = on_connect
     mqttc.on_publish = on_publish
 
+    mqttc.username_pw_set("/subscriber:publisher", "publisher_pass")
+
+    if len(sys.argv) > 1:
+        topic = "topic/string/" + sys.argv[1]
+    else:
+        topic = "topic/string/0"
+
     mqttc.connect("192.168.33.11", 1883, 60)
 
-    mqttc.publish("my/topic/string", "hello world", 1)
+    mqttc.publish(topic, "hello world", 1)
--- sub.py
+++ sub.py
@@ -1,19 +1,22 @@
 # coding=utf8
 
 import paho.mqtt.client as paho
+import sys
+
+topic = "$SYS/#"
 
 def on_connect(mqttc, obj, rc):
-    mqttc.subscribe("$SYS/#", 0)
-    print("rc: "+str(rc))
+    # mqttc.subscribe(topic, 0)
+    print("subscribe " + topic + " rc: " + str(rc))
 
 def on_message(mqttc, obj, msg):
-    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
 def on_publish(mqttc, obj, mid):
-    print("mid: "+str(mid))
+    print("mid: " + str(mid))
 
 def on_subscribe(mqttc, obj, mid, granted_qos):
-    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
 def on_log(mqttc, obj, level, string):
     print(string)
@@ -24,8 +27,15 @@ if __name__ == '__main__':
     mqttc.on_connect = on_connect
     mqttc.on_subscribe = on_subscribe
 
+    mqttc.username_pw_set("/subscriber:subscriber", "subscriber_pass")
+
+    if len(sys.argv) > 1:
+        topic = "topic/string/" + sys.argv[1]
+    else:
+        topic = "topic/string/#"
+
     mqttc.connect("192.168.33.12", 1883, 60)
 
-    mqttc.subscribe("my/topic/string", 0)
+    mqttc.subscribe(topic, 0)
 
     mqttc.loop_forever()
